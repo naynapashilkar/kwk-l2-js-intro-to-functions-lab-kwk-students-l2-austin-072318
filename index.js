d3f4523994c4d90d49eeedb7acd3efe21bc23b95
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-25 18:31:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Write your code in this file!
+function getUKTime() {
+let ukTime =new Date().getHours()+ 6
+ return ukTime
+}
 
+function discountTicket (originalPrice) {
+  let newPrice = originalPrice*0.8
+  return newPrice
+}
 
+function tweetReply (fan) {
+  return "Hey "+ fan +", thanks so much for the support! I really appreciate it!"
+}
